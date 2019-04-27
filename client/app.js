6c7e22a648dd6ded001f91a8686c25149c6e87da
Bowe Strickland <bowe@redhat.com>
2019-04-26 23:45:59 -0400

    fix board port

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8089
+const port = 8080
 
 app.use('/board', express.static('dist'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
